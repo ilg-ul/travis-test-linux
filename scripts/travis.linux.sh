b4a58aada38a6f702b908ca22d027a1631778a95
Liviu Ionescu <ilg@livius.net>
2017-02-03 17:44:51 +0200

    substitution

--- scripts/travis.linux.sh
+++ scripts/travis.linux.sh
@@ -23,6 +23,8 @@ site="${HOME}/${GITHUB_REPO}.git"
 function do_before_install() {
 
   cd ${HOME}
+  ls -lL ${TRAVIS_REPO_SLUG}
+  
   # gem install html-proofer
   # htmlproofer --version
 
@@ -38,16 +40,30 @@ function do_before_script() {
   git config --global user.email "ilg@livius.net"
   git config --global user.name "Liviu Ionescu (Travis CI)"
 
-  git clone -b master https://github.com/ilg-ul/travis-test-jekyll.git ilg-ul/travis-test-jekyll.git
+  git clone -b master https://github.com/${GITHUB_REPO}.git ${GITHUB_REPO}.git
 
   return 0
 }
 
 function do_script() {
 
+  cd ${HOME}/${TRAVIS_REPO_SLUG}
   ls -lL
+
   bundle exec jekyll build --destination ${site}
   # bundle exec htmlproofer ${site}
+
+  cd ${site}
+
+  git add --all .
+  git commit -m "Deploy to Github Pages"
+  env
+
+  git status
+
+  git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master
+
+  if [ "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
 
   return 0
 }
@@ -57,12 +73,12 @@ function do_after_success() {
 
   cd ${site}
 
-  git add .
+  git add --all .
   git commit -m "Deploy to Github Pages"
   env
 
   git status
-  
+
   git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master
 
   return 0
