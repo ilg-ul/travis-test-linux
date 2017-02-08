47ad65011d304167c47a8c8585ac71128ee0b067
Liviu Ionescu <ilg@livius.net>
2017-02-08 18:43:11 +0200

    remove doxygen

--- scripts/travis.linux.sh
+++ scripts/travis.linux.sh
@@ -53,14 +53,15 @@ function do_before_install() {
 
   # do_run doxygen --version
 
-  do_run sudo add-apt-repository -y ppa:clearpath-robotics/docs
-  do_run sudo apt-get -y -q update
-  do_run sudo apt-get -y install doxygen
+  # do_run sudo add-apt-repository -y ppa:clearpath-robotics/docs
+  # do_run sudo apt-get -y -q update
+  # do_run sudo apt-get -y install doxygen
 
   mkdir -p ${HOME}/downloads
 
   do_run curl -L https://launchpad.net/ubuntu/+archive/primary/+files/doxygen-dbg_1.8.11-3_amd64.deb -o ${HOME}/downloads/doxygen-dbg_1.8.11-3_amd64.deb
 
+  do_run apt-get remove doxygen
   do_run sudo dpkg -i ${HOME}/downloads/doxygen-dbg_1.8.11-3_amd64.deb
 
   do_run doxygen --version
